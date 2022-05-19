ea4a227a969d623a8617d39f9c05cded5d90f9ee
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-05-19 17:02:22 -0300

    Integrated microfrontends

--- ecomm/container/webpack.config.js
+++ ecomm/container/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   mode: 'development',
@@ -6,6 +7,12 @@ module.exports = {
     port: 8080
   },
   plugins: [
+    new ModuleFederation({
+      name: 'container',
+      remotes: {
+        products: 'products@http://localhost:8081/remoteEntry.js'
+      }
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
