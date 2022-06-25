930af5f6a3f5cc26d68b388f0d93b1aac12247b9
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-06-25 15:48:28 -0300

    Integration of Container and Marketing

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { mount as mountMarketingApp } from 'marketing/MarketingApp';
+
+console.log(mountMarketingApp)
 
 const App = () => {
   return <h1>Container</h1>;
